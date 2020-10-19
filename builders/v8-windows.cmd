ab4ff1c93b8dcf83cfd7b025746f0a691565c3b0
johnche <johnche@tencent.com>
2020-10-19 14:17:14 +0800

    v8-8.4.371.22f1

--- builders/v8-windows.cmd
+++ builders/v8-windows.cmd
@@ -6,7 +6,7 @@ git config --global core.autocrlf false
 git config --global core.filemode false
 git config --global color.ui true
 
-dir %GITHUB_WORKSPACE%\v8-8.6.393\third_party\inspector_protocol\
+dir %GITHUB_WORKSPACE%\v8-8.4.371.22f1\
 
 cd %HOMEPATH%
 echo =====[ Getting Depot Tools ]=====
@@ -31,7 +31,7 @@ cd v8
 call git checkout %VERSION%
 call gclient sync
 
-copy %GITHUB_WORKSPACE%\v8-8.6.348\* .\
+copy %GITHUB_WORKSPACE%\v8-8.4.371.22f1\* .\
 
 
 echo =====[ Building V8 ]=====
